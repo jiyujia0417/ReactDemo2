4f7a71b313ead0dd12e8c62fc739948924ae47b3
jiyujia0417 <jiyujia0417@163.com>
2020-11-06 13:58:39 +0800

    修改删除时始种删掉第一个的bug

--- src/TaoBaoPages/reducer/shoucang.js
+++ src/TaoBaoPages/reducer/shoucang.js
@@ -4,14 +4,13 @@ let shoucang = []
 const shoucanglist = (state = shoucang, action) => {
     //add shoucang
     if (action.type === 'ADD_SHOUCANG') {
-        console.log(action)
         return [...state, action.value]
     }
 
     //delete shoucang
     if (action.type === 'DEL_SHOUCANG') {
         let arr = [...state];
-        arr.splice(action.index, 1)
+        arr = arr.filter((item) => item != action.value)
         return arr;
     }
     return state;
